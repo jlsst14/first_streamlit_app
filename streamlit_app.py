be295f3042e8a395acdc3b671e9b55adb261782d
jlsst14 <131993549+jlsst14@users.noreply.github.com>
2023-05-02 16:05:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ def get_fruityvice_data(this_fruit_choice):
     return fruityvice_normalized
 
 #New section to display fruityvice api response
-streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header('Fruityvice Fruit Advice!')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
@@ -50,7 +50,7 @@ streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
-        my_cur_execute("select * from fruit_load_list")
+        my_cur.execute("select * from fruit_load_list")
     return my_cur.fetchall()
     
 #Add a button to load the fruit
