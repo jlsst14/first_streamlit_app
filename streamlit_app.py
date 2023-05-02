9fd71c4bccf1e6add060607f2ed68a21ab1e4cca
jlsst14 <131993549+jlsst14@users.noreply.github.com>
2023-05-02 13:16:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,7 +73,5 @@ if streamlit.button('Add a fruit to the list'):
     back_from_function = insert_row_snowflake(add_my_fruit)
     streamlit.text(back_from_function)
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-
 #don't run anything past here while we troubleshoot
 streamlit.stop()
